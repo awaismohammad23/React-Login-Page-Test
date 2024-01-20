35832acaa0dae315310310e33b0e8ad3ca70f770
Awais Mohammad <awaismadad@gmail.com>
2024-01-20 22:39:32 +0500

    pictures prepared to export assets in the index.js file.

---
+++ src/assets/index.js
@@ -0,0 +1,43 @@
+import Background from './background.png'
+import HelpInformation from 'help-information.svg'
+import Hotel1 from 'hotel1.png'
+import Hotel2 from 'hotel2.png'
+import Hotel3 from 'hotel3.png'
+import Hotel4 from 'hotel4.png'
+import Hotel5 from 'hotel5.png'
+import Hotel6 from 'hotel6.png'
+import Hotel7 from 'hotel7.png'
+import Hotel8 from 'hotel8.png'
+import MainLogo from 'Logo-1.svg'
+import SecondLogo from 'Logo.svg'
+import Logout from 'logout.svg'
+import Overview from 'overview.svg'
+import Profile from 'profile.svg'
+import Quotation from 'quotation.svg'
+import ROE from 'ROE.svg'
+import SightSeeings from 'sight-seeings.svg'
+import Transfers from 'transfers.svg'
+
+//exporting the logos and background images for the components.
+export {
+
+    Background,
+    HelpInformation,
+    Hotel1,
+    Hotel2,
+    Hotel3,
+    Hotel4,
+    Hotel5,
+    Hotel6,
+    Hotel7,
+    Hotel8,
+    MainLogo,
+    SecondLogo,
+    Logout,
+    Overview,
+    Profile,
+    Quotation,
+    ROE,
+    SightSeeings,
+    Transfers
+}
