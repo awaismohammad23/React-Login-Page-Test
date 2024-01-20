ff212f5751cb960c842c45d138f90d6314795469
Awais Mohammad <awaismadad@gmail.com>
2024-01-20 21:21:09 +0500

    ui component creation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './frontend/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <LoginPage/>
+    
+    </div>
+  );
+}
+
+export default App;
