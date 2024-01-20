effa76d42dac99383932a892f9a7581127e12738
Awais Mohammad <awaismadad@gmail.com>
2024-01-21 02:53:09 +0500

    Login Screen completed and second screen started

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,8 +5,10 @@ import Overview from '../../assets/overview.svg';
 import Quotation from '../../assets/quotation.svg';
 import Hotels from '../../assets/hotels.svg';
 import SightSeeings from '../../assets/sight-seeings.svg';
-
-
+import Transfers from '../../assets/transfers.svg';
+import ROE from '../../assets/ROE.svg';
+import HelpAndInformation from '../../assets/help-information.svg';
+import Logout from '../../assets/logout.svg';
 
 export const Sidebar = () => {
   return (
@@ -26,25 +28,47 @@ export const Sidebar = () => {
 
             <div className='items'>
 
-               <img src={Quotation} alt="overview" /> <span> Quotation</span>
+               <img src={Quotation} alt="quotations" /> <span> Quotation</span>
 
             </div>
             <div className='items'>
 
-               <img src={Hotels} alt="overview" /> <span> Hotels</span>
+               <img src={Hotels} alt="hotels" /> <span> Hotels</span>
 
             </div>
             <div className='items'>
 
-               <img src={SightSeeings} alt="overview" /> <span> Sight Seeings</span>
+               <img src={SightSeeings} alt="signt-seeings" /> <span> Sight Seeings</span>
 
             </div>
             <div className='items'>
 
-               <img src={Overview} alt="overview" /> <span> Overview</span>
+               <img src={Transfers} alt="transfers" /> <span> Transfers</span>
+
+            </div>
+            <div className='items'>
+
+               <img src={ROE} alt="roe" /> <span> ROE</span>
 
             </div>
 
+            <div className='items'></div>
+            <div className='items'></div>
+            <div className='items'></div>
+            <div className='items'></div>
+            <div className='items'></div>
+
+            <div className='items'>
+
+                <img src={HelpAndInformation} alt="roe" /> <span> Help and Information</span>
+
+            </div>
+
+            <div className='items'>
+
+                <img src={Logout} alt="roe" /> <span> Logout</span>
+
+            </div>
         
         </div>
    
