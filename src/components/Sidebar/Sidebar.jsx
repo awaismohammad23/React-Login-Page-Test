bb665b2ee5a5025d1d384a1582f638ea114f1b81
Awais Mohammad <awaismadad@gmail.com>
2024-01-22 03:19:22 +0500

    UI completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -132,6 +132,7 @@ export const Sidebar = () => {
           <div className='items'></div>
           <div className='items'></div>
           <div className='items'></div>
+          <div className='items'></div>
 
 
           <div className='items'>
